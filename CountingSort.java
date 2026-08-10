8dc830943e976c353b5464291f6d56d09a907d18
T.Nandhini Reddy <242t605119@ashokacollege.in>
2026-08-10 15:16:49 +0100

    Implement Counting Sort algorithm in Java

---
+++ CountingSort.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class CountingSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " non-negative elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        int[] count = new int[max + 1];
+
+        for (int i = 0; i < n; i++) {
+            count[arr[i]]++;
+        }
+
+        int index = 0;
+
+        for (int i = 0; i <= max; i++) {
+            while (count[i] > 0) {
+                arr[index] = i;
+                index++;
+                count[i]--;
+            }
+        }
+
+        System.out.println("Sorted array:");
+
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+
+        sc.close();
+    }
+}
